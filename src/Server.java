8554edebca2591f83437416275ef92b11548cd0b
郑勤 <10205501439@stu.ecnu.edu.cn>
2022-06-18 14:14:01 +0800

    fix: bug && feat: PUT method

--- src/Server.java
+++ src/Server.java
@@ -14,12 +14,12 @@ public class Server {
 
 
     public static void main( String[] args ) throws IOException {
-        ExecutorService ThreadPool = Executors.newFixedThreadPool(200);
+        ExecutorService ThreadPool = Executors.newFixedThreadPool(200); //开辟一个固定大小的线程池
         ServerSocket serverSocket = new ServerSocket(PORT);
         while (true){
-            Socket socket = serverSocket.accept();
-            System.out.println("Success");
-            ThreadPool.execute(new Handler(socket));
+            Socket socket = serverSocket.accept(); //监听固定端口是否有客户端连接
+            System.out.println("Success"); //检测到连接则print成功
+            ThreadPool.execute(new Handler(socket)); //使用线程池调用一个线程处理连接
         }
     }
 
@@ -34,6 +34,7 @@ class Handler implements Runnable{
     private static ResponseHeader responseHeader;
 
     Handler(Socket socket){
+
         this.socket = socket;
     }
 
@@ -46,7 +47,7 @@ class Handler implements Runnable{
         }
     }
 
-    static void handle500( Socket socket ) {
+    private static void handle500( Socket socket ) {
         try{
             handleError(socket, 500);
         }
@@ -55,7 +56,7 @@ class Handler implements Runnable{
         }
     }
 
-    static void handle404( Socket socket ) {
+    private static void handle404( Socket socket ) {
         try{
             handleError(socket, 404);
         }
@@ -66,24 +67,24 @@ class Handler implements Runnable{
 
     private static void handleError( @NotNull Socket socket ,int code ) throws IOException {
 
-        String filename = "web/error/" + code + ".html";
-        byte[] responseBody = Utils.NIOReadFile(filename);
-        Utils.writeResponse(responseHeader, code, responseBody.length, filename);
+        String filename = "web/error/" + code + ".html"; //确定响应页面
+        byte[] responseBody = Utils.NIOReadFile(filename); //获取响应报文数据
+        Utils.writeResponse(responseHeader, code, responseBody.length, filename); //生成响应报文头
         socket.getOutputStream().write(responseHeader.toString().getBytes(StandardCharsets.UTF_8));
-        socket.getOutputStream().write(responseBody);
+        socket.getOutputStream().write(responseBody); //回复响应报文头及数据
 
     }
 
     private static void handle200( Socket socket ) throws IOException {
 
-        String method = requestHeader.getMethod();
+        String method = requestHeader.getMethod(); //获取请求方法
 
         switch (method){
             case "GET":
-                String url = requestHeader.getUrl();
+                String url = requestHeader.getUrl(); //获取请求的url
 
-                if(url.equals(Utils.EXIT))
-                    System.exit(-1);
+                if(url.equals(Utils.EXIT)) //if shutdown
+                    System.exit(-1); //程序退出，关闭服务器
 
                 byte[] responseBody = Utils.NIOReadFile(url);
 
@@ -99,24 +100,33 @@ class Handler implements Runnable{
                 Utils.writeResponse(responseHeader, 200);
                 responseHeader.setContent_type("");
                 responseHeader.setContent_length(0);
-                String data = requestHeader.getData();
-                Utils.NIOWriteFile("db/data.txt", data);
+                String data = requestHeader.getData(); //获取post的内容
+                Utils.NIOWriteFile("db/data.txt", data, requestHeader.getContent_length()); //将data写入数据库db（伪）
                 socket.getOutputStream().write(responseHeader.toString().getBytes(StandardCharsets.UTF_8));
                 break;
+            case "PUT":
+                Utils.writeResponse(responseHeader, 200);
+                responseHeader.setContent_type(Utils.queryFileType(".html"));
+                responseBody = Utils.mdToHtml(responseHeader.getData()).getBytes(StandardCharsets.UTF_8);
+                //实现将md文件转换成html（读取请求报文体内容，转换成html并转换成字节数组）
+                requestHeader.setContent_length(responseBody.length); //获取字节数组长度
+                socket.getOutputStream().write(responseHeader.toString().getBytes(StandardCharsets.UTF_8));
+                socket.getOutputStream().write(responseBody);
+                break;
             default:
-                handle501(socket);
+                handle501(socket); //未实现
         }
 
     }
 
-    private @NotNull String getMsg( @NotNull BufferedReader reader) throws IOException {
-        StringBuilder ret = new StringBuilder();
+    private @NotNull String getMsg( @NotNull BufferedReader br) throws IOException {
+        StringBuilder ret = new StringBuilder(); //创建一个可变的字符序列
         char[] chars = new char[Utils.SIZE];
         do{
-            reader.read(chars);
+            br.read(chars);
             ret.append(chars);
-            Arrays.fill(chars, '\0');
-        } while (reader.ready());
+            Arrays.fill(chars, '\0'); //clear chars
+        } while (br.ready());
         return ret.toString();
     }
 
@@ -125,20 +135,20 @@ class Handler implements Runnable{
         try{
 
             InputStream is = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-            String temp = getMsg(reader);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String temp = getMsg(br); //将报文转为字符串
 
-            requestHeader = Utils.requestParseString(temp);
-            responseHeader = new ResponseHeader(requestHeader);
+            requestHeader = Utils.requestParseString(temp); //解析报文
+            responseHeader = new ResponseHeader(requestHeader); //创建报文头
 
             try {
-                Handler.handle200(socket);
+                Handler.handle200(socket); //请求成功
             }
             catch (FileNotFoundException e){
-                Handler.handle404(socket);
+                Handler.handle404(socket); //not found
             }
             catch (Exception e){
-                Handler.handle500(socket);
+                Handler.handle500(socket); //服务器错误
             }
             finally {
                 socket.close();
--- src/Utils.java
+++ src/Utils.java
@@ -1,7 +1,8 @@
-import com.google.gson.Gson;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
@@ -17,15 +18,17 @@ public class Utils {
     public static final String EXIT = "web/request//shutdown";
     public static final int SIZE = 1024;
 
-    private static final Pattern HOST = Pattern.compile("Host.*");
+    private static final Pattern HOST = Pattern.compile("Host.*"); //匹配关心内容的正则
 
     private static final Pattern CONTENT_LENGTH = Pattern.compile("Content-Length.*");
 
     private static final Pattern CONTENT_TYPE = Pattern.compile("Content-Type.*");
 
-    public static String beanToJSONString(Object bean){
-        Gson gson = new Gson();
-        return gson.toJson(bean);
+    public static String mdToHtml(String md){ //调包
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(md);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
     }
 
     public static @NotNull RequestHeader requestParseString( @NotNull String temp){
@@ -34,28 +37,29 @@ public class Utils {
 
         RequestHeader requestHeader = new RequestHeader();
 
-        String firstLine = temp.substring(0, temp.indexOf(CRLF));
+        String firstLine = temp.substring(0, temp.indexOf(CRLF)); //提取出请求行
         String[] parts = firstLine.split(" ");
 
         assert parts.length == 3;
 
         requestHeader.setMethod(parts[0]);
         requestHeader.setUrl(parts[1]);
-        requestHeader.setVersion(parts[2]);
+        requestHeader.setVersion(parts[2]); //分隔出请求方法，请求url，版本号
 
-        parts = temp.split(CRLF);
+        parts = temp.split(CRLF); //分隔请求报文的每一行
 
-        int split = -1;
-        for(int i = 0; i < parts.length; i++){
-            if(parts[i].equals("")){
-                split = i;
+        int split = 0; //为标志报文体开始位置
+        for(int i = 0; i < parts.length; i++){ //分隔每一请求报文首部行
+            if(parts[i].equals("")){ //为空则读至报文头和报文体的分界
+                split += 2; //跳过\r\n
                 break;
             }
+            split += (2 + parts.length); //增加\r\n与首部行长度
             int idx = parts[i].indexOf(":");
             if(idx == -1)
-                continue;
+                continue; //去除第一行
             if(Utils.HOST.matcher(parts[i]).matches())
-                requestHeader.setHost(parts[i].substring(idx + 2));
+                requestHeader.setHost(parts[i].substring(idx + 2)); //匹配首部名为host的值
 
             else if(Utils.CONTENT_LENGTH.matcher(parts[i]).matches())
                 requestHeader.setContent_length(Integer.parseInt(parts[i].substring(idx + 2)));
@@ -63,66 +67,70 @@ public class Utils {
             else if(Utils.CONTENT_TYPE.matcher(parts[i]).matches())
                 requestHeader.setContent_type(parts[i].substring(idx + 2));
             else {
-                String K = parts[i].substring(0 ,idx);
+                String K = parts[i].substring(0 ,idx); //key 为首部名
                 String V = "";
                 if (idx + 1 < parts[i].length())
-                    V = parts[i].substring(idx + 1);
+                    V = parts[i].substring(idx + 1); //value为首部值
                 requestHeader.putHeadMap(K ,V);
             }
         }
-        if(split > -1)
-            for(int i = split; i < parts.length; i++){
-                requestHeader.setData(parts[i]);
-            }
+        requestHeader.setData(temp.substring(split)); //读取请求报文体内容
 
         return requestHeader;
     }
 
     public static void writeResponse( @NotNull ResponseHeader header,int code,int length,String url){
-        header.setCode(code);
-        header.setContent_length(length);
-        header.setContent_type(Utils.queryFileType(url));
+        header.setCode(code); //设置状态码及含义
+        header.setContent_length(length); //设置长度
+        header.setContent_type(Utils.queryFileType(url)); //查询并设置类型
     }
 
     public static void writeResponse( @NotNull ResponseHeader header,int code){
+
         header.setCode(code);
     }
 
     public static byte @NotNull [] NIOReadFile( String url ) throws IOException {
-        RandomAccessFile access = new RandomAccessFile(url, "r");
-        FileChannel channel = access.getChannel();
-        ByteBuffer temp = ByteBuffer.allocate((int) access.length());
-        channel.read(temp);
-        temp.flip();
+        RandomAccessFile access = new RandomAccessFile(url, "r"); //随机读文件
+        FileChannel channel = access.getChannel(); //打开文件通道
+        ByteBuffer temp = ByteBuffer.allocate((int) access.length()); //申请缓冲区空间
+        channel.read(temp); //数据从通道写入缓冲区
+        temp.flip(); //反转读写模式，position变为0
         byte[] ret = new byte[temp.remaining()];
-        temp.get(ret, 0, ret.length);
+        temp.get(ret, 0, ret.length); //将缓冲区内数据写入ret数组
         return ret;
     }
 
-    public static void NIOWriteFile( String filename ,String data ) throws IOException {
+    public static void NIOWriteFile( String filename ,String data , int length ) throws IOException {
         RandomAccessFile access = new RandomAccessFile(filename, "rw");
 
         FileChannel channel = access.getChannel();
-        ByteBuffer byteBuffer = StandardCharsets.UTF_8.encode(data);
+
+        byte[] content = new byte[length];
+        byte[] dataBytes = data.getBytes(StandardCharsets.UTF_8);
+        System.arraycopy(dataBytes, 0, content, 0,  length); //将data中数据复制到定长的字节数组中，避免末尾出现null
+        ByteBuffer byteBuffer = ByteBuffer.wrap(content); //创建缓冲区
 
         channel.write(byteBuffer);
     }
 
     public static String queryCode(int code){
-        return CodeStatus.valueOf("STATUS_CODE_"+code).getStatus();
+        return CodeStatus.valueOf("STATUS_CODE_"+code).getStatus(); //查询状态码对应含义
     }
 
     public static String queryFileType( @NotNull String url){
-        String type = url.split("\\.")[1].toLowerCase();
-        return FileType.valueOf(type).getType();
+        String type = url.split("\\.")[1].toLowerCase(); //获得类型字符串
+        return FileType.valueOf(type).getType(); //返回对应类型
     }
 
 }
 
 enum CodeStatus{
 
-    STATUS_CODE_200("OK", 200), STATUS_CODE_404("Not Found", 404),
-    STATUS_CODE_501("Not Implemented", 501),STATUS_CODE_500 ("Internal Server Error",500);
+    STATUS_CODE_200("OK", 200),
+    STATUS_CODE_404("Not Found", 404),
+    STATUS_CODE_501("Not Implemented", 501),
+    STATUS_CODE_500 ("Internal Server Error",500);
 
     private int code;
     private String status;
